b714961d75aadc64917ef9f5fcb570a2a17dff43
Brian Phiri <bph5nai@bosch.com>
2025-03-07 14:48:47 +0300

    make it work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,27 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import rehypePrettyCode from 'rehype-pretty-code';
-import { siteConfig } from './src/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import rehypePrettyCode from "rehype-pretty-code";
+import { siteConfig } from "./src/config";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: siteConfig.site,
+  site: "https://github.com/BrianPhiri/my-blog",
+  base: "/my-blog",
   integrations: [tailwind(), sitemap()],
   markdown: {
     rehypePlugins: [
-      [rehypePrettyCode, {
-        theme: 'github-dark',
-        onVisitLine(node) {
-          if (node.children.length === 0) {
-            node.children = [{type: 'text', value: ' '}];
-          }
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(node) {
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          },
         },
-      }],
+      ],
     ],
   },
 });
